995fdb9350c69b81f2be1dfc88bf16f2673c0b42
Kenneth Pinkerton <kenneth.pinkerton90@gmail.com>
2024-10-04 12:51:30 -0500

    Update Portfolio section organization and CSS to allow for multiple projects to be presented in the portfolio section

--- src/assets/components/Portfolio.jsx
+++ src/assets/components/Portfolio.jsx
@@ -8,7 +8,7 @@ const Portfolio = () => {
 			id="portfolio"
 			className="portfolio section-bg"
 		>
-			<div className="container">
+			<div className="container-fluid">
 				<div className="section-title">
 					<h2>Portfolio</h2>
 					<p className="summary">
@@ -23,75 +23,234 @@ const Portfolio = () => {
 					</p>
 				</div>
 
-				<Fade className="row"></Fade>
-
-				<div className="col-lg-4 col-md-6 portfolio-item filter-web">
-					<div className="portfolio-wrap">
-						<img
-							src={ToDoGif}
-							className="img-fluid"
-							alt="Animated demonstration of ToDo App"
-						/>
-						<div className="portfolio-links">
-							<a
-								href="https://seraphimsage.github.io/todo-app-part-2/"
-								className="portfolio-lightbox"
-								target="_blank"
-								title="App Deployment"
-							>
-								<Icon.AppIndicator />
-							</a>
-							<a
-								href="https://github.com/SeraphimSage/todo-app-part-2"
-								className="portfolio-lightbox"
-								target="_blank"
-								title="App Repo"
-							>
-								<Icon.App />
-							</a>
-						</div>
-					</div>
-					<div className="col-lg-12">
-						<div className="portfolio-info">
-							<h3>Project information</h3>
-							<ul>
-								<li>
-									<strong>Category</strong>: Web design
-								</li>
-								<li>
-									<strong>Client</strong>: Kenzie Academy
-								</li>
-								<li>
-									<strong>Project date</strong>: Feb 15, 2020
-								</li>
-								<li>
-									<strong>Project URL</strong>:
+				<Fade>
+					<div className="row">
+						{/* First Portfolio Item */}
+						<div className="col-lg-6 col-md-6 portfolio-item">
+							<div className="portfolio-wrap">
+								<img
+									src={ToDoGif}
+									className="img-fluid"
+									alt="Animated demonstration of ToDo App"
+								/>
+								<div className="portfolio-links">
 									<a
 										href="https://seraphimsage.github.io/todo-app-part-2/"
+										className="portfolio-lightbox"
 										target="_blank"
 										title="App Deployment"
 									>
-										https://seraphimsage.github.io/todo-app-part-2/
+										<Icon.AppIndicator />
 									</a>
-								</li>
-								<li>
-									<strong>Project Technologies</strong>: React, CSS, HTML
-								</li>
-							</ul>
+									<a
+										href="https://github.com/SeraphimSage/todo-app-part-2"
+										className="portfolio-lightbox"
+										target="_blank"
+										title="App Repo"
+									>
+										<Icon.App />
+									</a>
+								</div>
+							</div>
+							<div className="portfolio-info">
+								<h3>Project information</h3>
+								<ul>
+									<li>
+										<strong>Category</strong>: Web design
+									</li>
+									<li>
+										<strong>Client</strong>: Kenzie Academy
+									</li>
+									<li>
+										<strong>Project date</strong>: Feb 15, 2020
+									</li>
+									<li>
+										<strong>Project URL</strong>:{" "}
+										<a
+											href="https://seraphimsage.github.io/todo-app-part-2/"
+											target="_blank"
+											title="App Deployment"
+										>
+											https://seraphimsage.github.io/todo-app-part-2/
+										</a>
+									</li>
+									<li>
+										<strong>Project Technologies</strong>: React, CSS, HTML
+									</li>
+								</ul>
+							</div>
 						</div>
-						<div className="portfolio-description">
-							<h2>Project Details</h2>
-							<p>
-								This project was completed as part of a full-stack web
-								development educational course through Kenzie Academy.
-							</p>
-							<p>
-								The purpose of this project is to demonstrate how CRUD (Create,
-								Read, Update, and Delete) techniques can be used in creating a web app.
-							</p>
+
+						{/* Second Portfolio Item */}
+						<div className="col-lg-6 col-md-6 portfolio-item">
+							<div className="portfolio-wrap">
+								<img
+									src={ToDoGif}
+									className="img-fluid"
+									alt="Animated demonstration of ToDo App"
+								/>
+								<div className="portfolio-links">
+									<a
+										href="https://seraphimsage.github.io/todo-app-part-2/"
+										className="portfolio-lightbox"
+										target="_blank"
+										title="App Deployment"
+									>
+										<Icon.AppIndicator />
+									</a>
+									<a
+										href="https://github.com/SeraphimSage/todo-app-part-2"
+										className="portfolio-lightbox"
+										target="_blank"
+										title="App Repo"
+									>
+										<Icon.App />
+									</a>
+								</div>
+							</div>
+							<div className="portfolio-info">
+								<h3>Project information</h3>
+								<ul>
+									<li>
+										<strong>Category</strong>: Web design
+									</li>
+									<li>
+										<strong>Client</strong>: Kenzie Academy
+									</li>
+									<li>
+										<strong>Project date</strong>: Feb 15, 2020
+									</li>
+									<li>
+										<strong>Project URL</strong>:{" "}
+										<a
+											href="https://seraphimsage.github.io/todo-app-part-2/"
+											target="_blank"
+											title="App Deployment"
+										>
+											https://seraphimsage.github.io/todo-app-part-2/
+										</a>
+									</li>
+									<li>
+										<strong>Project Technologies</strong>: React, CSS, HTML
+									</li>
+								</ul>
+							</div>
 						</div>
 					</div>
-				</div>
+					<div className="row">
+						{/* First Portfolio Item */}
+						<div className="col-lg-6 col-md-6 portfolio-item">
+							<div className="portfolio-wrap">
+								<img
+									src={ToDoGif}
+									className="img-fluid"
+									alt="Animated demonstration of ToDo App"
+								/>
+								<div className="portfolio-links">
+									<a
+										href="https://seraphimsage.github.io/todo-app-part-2/"
+										className="portfolio-lightbox"
+										target="_blank"
+										title="App Deployment"
+									>
+										<Icon.AppIndicator />
+									</a>
+									<a
+										href="https://github.com/SeraphimSage/todo-app-part-2"
+										className="portfolio-lightbox"
+										target="_blank"
+										title="App Repo"
+									>
+										<Icon.App />
+									</a>
+								</div>
+							</div>
+							<div className="portfolio-info">
+								<h3>Project information</h3>
+								<ul>
+									<li>
+										<strong>Category</strong>: Web design
+									</li>
+									<li>
+										<strong>Client</strong>: Kenzie Academy
+									</li>
+									<li>
+										<strong>Project date</strong>: Feb 15, 2020
+									</li>
+									<li>
+										<strong>Project URL</strong>:{" "}
+										<a
+											href="https://seraphimsage.github.io/todo-app-part-2/"
+											target="_blank"
+											title="App Deployment"
+										>
+											https://seraphimsage.github.io/todo-app-part-2/
+										</a>
+									</li>
+									<li>
+										<strong>Project Technologies</strong>: React, CSS, HTML
+									</li>
+								</ul>
+							</div>
+						</div>
+
+						{/* Second Portfolio Item */}
+						<div className="col-lg-6 col-md-6 portfolio-item">
+							<div className="portfolio-wrap">
+								<img
+									src={ToDoGif}
+									className="img-fluid"
+									alt="Animated demonstration of ToDo App"
+								/>
+								<div className="portfolio-links">
+									<a
+										href="https://seraphimsage.github.io/todo-app-part-2/"
+										className="portfolio-lightbox"
+										target="_blank"
+										title="App Deployment"
+									>
+										<Icon.AppIndicator />
+									</a>
+									<a
+										href="https://github.com/SeraphimSage/todo-app-part-2"
+										className="portfolio-lightbox"
+										target="_blank"
+										title="App Repo"
+									>
+										<Icon.App />
+									</a>
+								</div>
+							</div>
+							<div className="portfolio-info">
+								<h3>Project information</h3>
+								<ul>
+									<li>
+										<strong>Category</strong>: Web design
+									</li>
+									<li>
+										<strong>Client</strong>: Kenzie Academy
+									</li>
+									<li>
+										<strong>Project date</strong>: Feb 15, 2020
+									</li>
+									<li>
+										<strong>Project URL</strong>:{" "}
+										<a
+											href="https://seraphimsage.github.io/todo-app-part-2/"
+											target="_blank"
+											title="App Deployment"
+										>
+											https://seraphimsage.github.io/todo-app-part-2/
+										</a>
+									</li>
+									<li>
+										<strong>Project Technologies</strong>: React, CSS, HTML
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</Fade>
 			</div>
 		</section>
 	);
