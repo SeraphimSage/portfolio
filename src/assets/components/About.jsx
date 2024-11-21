7bdf80d15eff531df508d3531f33ec71c1bda6f5
Kenneth Pinkerton <kenneth.pinkerton90@gmail.com>
2024-11-21 14:01:18 -0600

    update to add goals and hobbies section, minor update to formatting to make text less crowded.

--- src/assets/components/About.jsx
+++ src/assets/components/About.jsx
@@ -12,10 +12,14 @@ const About = () => {
 				<div className="section-title">
 					<h2>About</h2>
 					<p className="summary">
-						Dynamic Web Developer with a solid reputation for creative and
-						collaborative implementations of cutting-edge web applications.
-						Focused on full-system support to enhance team efficiency and reduce
-						operations time.
+						I&apos;m a dynamic Web Developer with a solid reputation for
+						creative and collaborative implementations of cutting-edge web
+						applications. Focused on full-system support to enhance team
+						efficiency and reduce operations time. My experience has primarily
+						been working with startups and non-profits. In these roles I oversaw
+						the development of the organizations online presence, and developed
+						long term strategy to help the respective organizations achieve
+						their long term growth targets.
 					</p>
 					<h3>Business Consultant &amp; Full-Stack Web Developer.</h3>
 					<div className="row">
@@ -61,6 +65,26 @@ const About = () => {
 											<strong>Freelance: </strong> <span>Available</span>
 										</li>
 									</ul>
+								</div>
+								<div className="row">
+									<div className="col-lg-12">
+										<h3>Goals</h3>
+										<p className="goals">
+											My overall career goals are to grow my business into the
+											space sector and continue my education to gain a Masters
+											in Space Systems Engineering.
+										</p>
+									</div>
+								</div>
+								<div className="row">
+									<div className="col-lg-12">
+										<h3>Hobbies</h3>
+										<p className="goals">
+											When I&apos;m not working, I enjoy spending time with my
+											family and friends, playing video games, practicing
+											martial arts, cooking, or hiking.
+										</p>
+									</div>
 								</div>
 							</div>
 						</Fade>
